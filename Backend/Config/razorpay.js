e9f0a80874e4390e124aefff5af98c5556642bd9
jaickeyminj <jaickey09@gmail.com>
2024-03-27 00:47:39 +0530

    RazorPay front as well as back

--- Backend/Config/razorpay.js
+++ Backend/Config/razorpay.js
@@ -1,8 +1,9 @@
 const Razorpay = require('razorpay'); 
+const { v4: uuid } = require("uuid");
 require("dotenv").config();
 // This razorpayInstance will be used to 
 // access any resource from razorpay 
-const razorpayInstance = new Razorpay({ 
+const razorpay = new Razorpay({ 
   
     // Replace with your key_id 
     key_id: process.env.RAZORPAY_KEY_ID, 
@@ -11,4 +12,20 @@ const razorpayInstance = new Razorpay({
     key_secret: process.env.RAZORPAY_KEY_SECRET 
   }); 
 
-module.exports = razorpayInstance;
+exports.createRazorpayOrder = (req, res) => {
+  console.log("create order");
+    const options = {
+        amount: req.body.amount, // amount in the smallest currency unit
+        currency: 'INR',
+        receipt: "receipt_order_",
+        payment_capture: 1
+    };
+    razorpay.orders.create(options, (err, order) => {
+        if (err) {
+            res.status(500).json({ error: err });
+            return;
+        }
+        console.log(order);
+        res.json(order);
+    });
+};
