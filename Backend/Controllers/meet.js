afcc18c437874614bff8e1ca0b7502645e1bb15a
jaickeyminj <jaickey09@gmail.com>
2024-03-26 12:33:31 +0530

    meet api with authorization

---
+++ Backend/Controllers/meet.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+const passport = require('passport');
+var GoogleStrategy = require('passport-google-oauth20').Strategy;
+const Meeting = require('google-meet-api').meet;
+
+
+clientID = "900631543829-0jjagfht4m75ehgv3in45elahvikhf6l.apps.googleusercontent.com"
+clientSecret="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"
+
+passport.use(new GoogleStrategy({
+    clientID: clientID,
+    clientSecret: clientSecret,
+    callbackURL: "http://localhost:8000/auth/callback"
+},
+    function (accessToken, refreshToken, profile, cb) {
+        Meeting({
+            clientId: clientID,
+            clientSecret: clientSecret,
+            refreshToken: refreshToken,
+            date: "2020-12-02",
+            time: "10:59",
+            summary: 'summary',
+            location: 'location',
+            description: 'description',
+            checking:0
+        }).then(function (result) {
+            console.log(result);
+        }).catch((error) => {
+            console.log(error)
+        });
+        // console.log(result);
+        return cb();
+    }
+));
+
+app.get('/auth/callback',
+    passport.authenticate('google', { failureRedirect: '/' })
+);
+
+app.get('/auth/google',
+    passport.authenticate('google', {
+        scope: ['profile','https://www.googleapis.com/auth/calendar'],
+        accessType: 'offline',
+        prompt: 'consent'
+    }
+    ));
+
+app.get('/',function(req,res){
+    res.send("done")
+})
+
+app.listen(port, function (err) {
+    if (err) {
+        console.log('something wrong in starting server !!!');
+        return;
+    }
+    return console.log("server is up and running on port ", port);
+});
